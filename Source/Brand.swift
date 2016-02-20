f8a20148cf6e43b91e16f694a57a1e2ff5e2f7de
Ekuklu, B <bugraekuklu@hotmail.de>
2016-02-20 17:46:36 +0200

    Added Brand ORM methods.

--- Source/Brand.swift
+++ Source/Brand.swift
@@ -21,19 +21,93 @@
 
 import Foundation
 import CoreGraphics
+import Alamofire
+import SwiftyJSON
 
 public class Brand : PersistentContext {
     //  Identity
     let name: String
-    let _colors: [CGColor]
+    let _colors: [[Float]]
     let products: [Product]?
     
-    init(identifier: String, name: String, _colors: [CGColor], products: [Product]?) {
+    var DBdescription: String?
+    
+    init(identifier: String, name: String, _colors: [[Float]], products: [Product]?) {
         self.name = name
         self._colors = _colors
         self.products = products
         
         super.init(identifier: identifier)
+    }
+    
+    private static func parse(json: JSON) -> Brand {
+        let identifier = json["_id"].stringValue
+        
+        let name = json["name"].stringValue
+        
+        var popularProducts = [Product]()
+        
+        for eachObject in json["popularProducts"] {
+            //  Create products and assign them.
+        }
+        
+        var colors = [[Float]]()
+        
+        if let colorArray = json["colors"].array {
+            for eachObject in colorArray {
+                var floats = [Float]()
+                
+                floats.append(eachObject["red"].floatValue / 256)
+                floats.append(eachObject["green"].floatValue / 256)
+                floats.append(eachObject["blue"].floatValue / 256)
+                
+                colors.append(floats)
+            }
+        }
+        
+        let brand = Brand(identifier: identifier, name: name, _colors: colors, products: popularProducts)
+        
+        brand.DBdescription = json["description"].string
+        
+        return brand
+    }
+    
+    static public func index(callback: ([Brand]?, NSError?) -> Void) {
+        Alamofire.request(.GET, "http://localhost:9000/api/brands")
+            .responseJSON { response in
+                switch response.result {
+                case .Success:
+                    if let value = response.result.value {
+                        var brands = [Brand]()
+                        
+                        if let array = JSON(value).array {
+                            for eachBrandJSON in array {
+                                brands.append(parse(eachBrandJSON))
+                            }
+                            
+                            callback(brands, nil)
+                        } else {
+                            callback(nil, NSError(domain: "LapsKit", code: 100, userInfo: ["description": "Malformed JSON"]))
+                        }
+                    }
+                case .Failure(let error):
+                    callback(nil, error)
+                }
+        }
+    }
+    
+    static public func show(callback: (Brand?, NSError?) -> Void) {
+        Alamofire.request(.GET, "http://localhost:9000/api/brands")
+            .responseJSON { response in
+                switch response.result {
+                case .Success:
+                    if let value = response.result.value {
+                        callback(parse(JSON(value)), nil)
+                    }
+                case .Failure(let error):
+                    callback(nil, error)
+                }
+        }
     }
     
     override func synchronize() {
